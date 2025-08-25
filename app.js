1d9258f1f7058a3d3ff2160c3abd1f275c77da91
NurulhudaAi <Nurulhudaadamishaq@gmail.com>
2025-08-25 16:32:49 +0700

    [backand]add endpoit for login, expenses by each userid, expenses by userid of today. [frontend] add print login, feature of 3 showTrackingApp,ShowAllExpenses,ShowTodayExpenses looping

--- app.js
+++ app.js
@@ -1,21 +1,66 @@
 const express = require('express');
 const app = express();
+const brcypt = require('bcrypt');
 const conn = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/expense', (req, res) => {
-  const sql = 'SELECT * FROM expenses';
-  conn.query(sql, (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.json(results);
-  });
-}); 
 
 const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server is running at ` + PORT);
 });
+
+// endpoint for login
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const sql = 'SELECT id, password FROM users WHERE username = ?';
+  conn.query(sql, [username], (err, results) => {
+    if (err) {
+      return res.status(500).send('Database error');
+    }
+    if (results.length !== 1) {
+      return res.status(401).send('User not found');
+    }
+    // compare passwords
+    brcypt.compare(password, results[0].password, (err, isMatch) => {
+      if (err) {
+        return res.status(500).send('Hashing error');
+      }
+      if (isMatch) {
+        return res.status(200).json({ userId: results[0].id });
+      }
+      return res.status(401).send('Wrong password');
+    });
+  });
+})
+
+
+//endpoint for each userid's expenses
+app.get('/expenses/:userId', (req, res) => {
+  const userId = req.params.userId;
+  const sql = 'SELECT item, paid, DATE_FORMAT(date, "%Y-%m-%d %H:%i:%s.000") as date FROM expense WHERE user_id = ?';
+
+
+  conn.query(sql, [userId], (err, results) => {
+    if (err) {
+      return res.status(500).send('Database error!');
+    }
+    res.status(200).json(results);
+  });
+});
+
+
+// endpoint for today's user expenses
+app.get('/expenses/:userId/today', (req, res) => {
+  const userId = req.params.userId;
+  const sql = 'SELECT item, paid, DATE_FORMAT(date, "%Y-%m-%d %H:%i:%s.000") as date FROM expense WHERE user_id = ? AND DATE(date) = CURDATE()';
+
+  conn.query(sql, [userId], (err, results) => {
+    if (err) {
+      return res.status(500).send('Database error!');
+    }
+    res.status(200).json(results);
+  });
+})
--- bin/dart_project.dart
+++ bin/dart_project.dart
@@ -1,3 +1,121 @@
-void main() {
-  print('Expense Tracking App');
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+void main() async {
+  print('===== Login =====');
+  // Get username and password
+  stdout.write('Username: ');
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write('Password: ');
+  String? password = stdin.readLineSync()?.trim();
+  // Check if username and password are not empty
+  if (username == null ||
+      password == null ||
+      username.isEmpty ||
+      password.isEmpty) {
+    print('Incomplete input');
+    return;
+  }
+
+  final body = {'username': username, 'password': password};
+  //URL
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+
+  if (response.statusCode == 200) {
+    final result = jsonDecode(response.body) as Map<String, dynamic>;
+    print(result);
+
+    // Get userId from response
+    final userId = result['userId'];
+    if (userId != null) {
+      await showTrackingApp(userId as int);
+    }
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    final result = response.body;
+    print(result);
+  } else {
+    print('Unknown error');
+  }
+}
+
+// fuction Show Menu
+Future<void> showTrackingApp(int userId) async {
+  while (true) {
+    print('================== Expenses Tracking App ==================');
+    print('1. Show all');
+    print('2. Today\'s expenses');
+    print('3. Exit');
+    stdout.write('Choose... ');
+    String? choice = stdin.readLineSync()?.trim();
+
+    switch (choice) {
+      case '1':
+        await showAllExpenses(userId);
+        break;
+      case '2':
+        await showTodayExpenses(userId);
+        break;
+      case '3':
+        print('----- Bye -----');
+        return;
+      default:
+        print('Invalid choice');
+    }
+  }
+}
+
+// Function to show all expenses
+Future<void> showAllExpenses(int userId) async {
+  final url = Uri.parse('http://localhost:3000/expenses/$userId');
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final result = jsonDecode(response.body) as List;
+
+    int total = 0;
+    print('---------------------- All expenses ----------------------');
+    for (int i = 0; i < result.length; i++) {
+      final exp = result[i];
+      final dt = DateTime.parse(exp["date"]);
+      final dtaLocal = dt.toLocal();
+      print(
+        '${i + 1}. ${exp["item"]} : ${exp["paid"]}฿ : ${dtaLocal.toString()}',
+      );
+      total += exp['paid'] as int;
+    }
+    print('Total expenses = $total฿');
+  } else if (response.statusCode == 404) {
+    print('No expenses found.');
+  } else {
+    print('Error: ${response.statusCode}');
+  }
+}
+
+// Function to show today's expenses
+Future<void> showTodayExpenses(int userId) async {
+  final url = Uri.parse('http://localhost:3000/expenses/$userId/today');
+  final response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    final result = jsonDecode(response.body) as List;
+
+    int total = 0;
+    print('---------------------- Today\'s expenses ----------------------');
+    for (int i = 0; i < result.length; i++) {
+      final exp = result[i];
+      final dt = DateTime.parse(exp["date"]);
+      final dtaLocal = dt.toLocal();
+      print(
+        '${i + 1}. ${exp["item"]} : ${exp["paid"]}฿ : ${dtaLocal.toString()}',
+      );
+      total += exp['paid'] as int;
+    }
+    print('Total expenses = $total฿');
+  } else if (response.statusCode == 404) {
+    print('No expenses found for today.');
+  } else {
+    print('Error: ${response.statusCode}');
+  }
 }
